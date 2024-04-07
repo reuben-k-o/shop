7c08ee412cfd0ec35b8969fa815f2788c1f34ec6
reuben-k-o <rubenkhaemba@gmail.com>
2024-04-07 09:51:03 +0300

    setup chakra

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,9 +2,14 @@ import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
+import { Box, Flex } from "@chakra-ui/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return <div style={{ textAlign: "center" }}>E-commerce shop</div>;
+  return (
+    <Flex h="100vh" justify="center" align="center">
+      E-commerce shop
+    </Flex>
+  );
 }
