fff557c8c8bc08e0c9a2b9c5433e8de16e46d6d3
reuben-k-o <rubenkhaemba@gmail.com>
2024-04-07 23:56:17 +0300

    added sidebar

---
+++ src/components/sidebar.tsx
@@ -0,0 +1,11 @@
+import { Box, Text } from "@chakra-ui/react";
+
+export default function SideBar() {
+  return (
+    <Box w="40vw">
+      <Text>All products</Text>
+      <Text>Some products</Text>
+      <Text>New products</Text>
+    </Box>
+  );
+}
