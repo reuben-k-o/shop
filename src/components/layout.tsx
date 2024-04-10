6c72a04c34a4e3dcca2d35e7907da6c29054f1f2
reuben-k-o <rubenkhaemba@gmail.com>
2024-04-10 23:33:28 +0300

    displaying flex

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -7,8 +7,7 @@ export default function Layout(props: { children: ReactNode }) {
   return (
     <Box>
       <Navigation />
-
-      <main>
+      <main style={{ display: "flex", height: "100vh", width: "100vw" }}>
         <SideBar />
         {props.children}
       </main>
