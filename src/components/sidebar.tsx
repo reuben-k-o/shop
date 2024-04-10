7410a4756f5079e48ff0a0dcfc73c5fee7c8a9f5
reuben-k-o <rubenkhaemba@gmail.com>
2024-04-10 23:33:06 +0300

    displaying items side by side

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -2,7 +2,7 @@ import { Box, Text } from "@chakra-ui/react";
 
 export default function SideBar() {
   return (
-    <Box w="40vw">
+    <Box w="12vw" h="100%" minH="100vh" ml="1rem" mt="1.5rem" borderRight="1px solid black">
       <Text>All products</Text>
       <Text>Some products</Text>
       <Text>New products</Text>
